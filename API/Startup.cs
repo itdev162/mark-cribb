3f2f9e0d9b1f029ada9a9fd3b12b4758258a2d46
Mark <cribbm@matc.edu>
2021-10-27 19:54:48 -0500

    Completed Activity 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -22,11 +22,22 @@ namespace API
             Configuration = configuration;
         }
 
+        private readonly string CorsPolicy = "CorsPolicy";
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy(CorsPolicy, policyBuilder =>
+                {
+                    policyBuilder
+                        .WithOrigins("http://localhost:3000")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+            });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddDbContext<DataContext>(opt =>
             {
@@ -48,6 +59,7 @@ namespace API
             }
 
             // app.UseHttpsRedirection();
+            app.UseCors(CorsPolicy);
             app.UseMvc();
         }
     }

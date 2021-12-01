7264725ce7190fc4caab75a3090a28a31b61f19f
Mark <cribbm@matc.edu>
2021-12-01 17:44:29 -0600

    Completed Activity 6

--- API/Controllers/PostsController.cs
+++ API/Controllers/PostsController.cs
@@ -5,6 +5,8 @@ using Application.Posts;
 using Domain;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Persistence;
+using System.Linq;
 
 namespace API.Controllers
 {
@@ -14,13 +16,78 @@ namespace API.Controllers
     public class PostsController : ControllerBase
     {
         private readonly IMediator mediator;
+        private readonly DataContext context;
 
-        public PostsController(IMediator mediator) => this.mediator = mediator;
+        public PostsController(IMediator mediator, DataContext context)
+        {
+            this.mediator = mediator;
+            this.context = context;
+        }
+
 
         public async Task<ActionResult<List<Post>>> List()
         {
             return await this.mediator.Send(new List.Query());
         }
+
+        [HttpGet]
+        public ActionResult<List<Post>> Get()
+        {
+            return this.context.Posts.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Post> GetById(Guid id)
+        {
+            return this.context.Posts.Find(id);
+        }
+
+        [HttpPost]
+        public ActionResult<Post> Create([FromBody]Post request)
+        {
+            var post = new Post
+            {
+                Id = request.Id,
+                Title = request.Title,
+                Body = request.Body,
+                Date = request.Date
+            };
+
+            context.Posts.Add(post);
+            var success = context.SaveChanges() > 0;
+
+            if (success)
+            {
+                return post;
+            }
+            
+            throw new Exception("Error creating post.");
+        }
+
+        [HttpPut]
+        public ActionResult<Post> Update([FromBody] Post request)
+        {
+            var post = context.Posts.Find(request.Id);
+
+            if (post == null)
+            {
+                throw new Exception("Could not find post.");
+            }
+
+            //Update the post properties with request values, if present.
+            post.Title = request.Title != null ? request.Title : post.Title;
+            post.Body = request.Body != null ? request.Body : post.Body;
+            post.Date = request.Date != null ? request.Date : post.Date;
+
+            var success = context.SaveChanges() > 0;
+
+            if (success)
+            {
+                return post;
+            }
+
+            throw new Exception("Error updating post.");
+        }        
 
     }
 }

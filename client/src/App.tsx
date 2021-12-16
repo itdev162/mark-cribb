d6fab38ea0e70ce6bfe34aeb0597c12d1d7fc29a
Mark <cribbm@matc.edu>
2021-12-15 19:44:50 -0600

    Completed Activity-10

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import axios from 'axios';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import PostList from './PostList/PostList';
 import Post from './Post/Post';
+import CreatePost from './Post/CreatePost';
+import EditPost from './Post/EditPost';
 import './App.css';
-
 
 
 
@@ -48,30 +49,65 @@ class App extends React.Component {
       });
   };
 
-  render() {
+  editPost = post => {
+    this.setState({
+      post: post
+    });
+  };
+
+  onPostCreated = post => {
+    const newPosts = [...this.state.posts, post];
+
+    this.setState({
+      posts: newPosts
+    });
+  };
+
+  onPostUpdated = post => {
+    console.log('updated post: ', post);
+    const newPosts = [...this.state.posts];
+    const index = newPosts.findIndex(p => p.id === post.id);
+
+    newPosts[index] = post;
+    
+    this.setState({
+      posts: newPosts
+    });
+  };
+
+render() {
     const { posts, post } = this.state;
 
-    return(
+    return (
       <Router>
         <div className="App">
-          <header className="App=header">
-            BlogBox
-          </header>
-          <main className="App-content">
-            <Switch>
-              <Route exact path="/">
-                <PostList
-                 posts={posts} 
-                 clickPost={this.viewPost}
-                 deletePost={this.deletePost}
-                  />
-              </Route>
-              <Route path="/posts/:postId">
-                <Post post={post} />
-              </Route>
-            </Switch>
-          </main>
-        </div>
+        <header className="App-header">BlogBox</header>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/new-post">New Post</Link>
+        </nav>
+        <main className="App-content">
+          <Switch>
+            <Route exact path="/">
+              <PostList 
+                posts={posts} 
+                clickPost={this.viewPost}
+                deletePost={this.deletePost} 
+                editPost={this.editPost}
+              />
+            </Route>
+            <Route path="/posts/:postId">
+              <Post post={post} />
+            </Route>
+            <Route path="/new-post">
+              <CreatePost onPostCreated={this.onPostCreated} />
+            </Route>
+            <Route path="/edit-post/:postId">
+              <EditPost post={post} onPostUpdated={this.onPostUpdated} />
+            </Route>
+          </Switch>
+        </main>
+      </div>
       </Router>
     );
   }
--- client/src/PostList/PostList.js
+++ client/src/PostList/PostList.js
@@ -2,15 +2,16 @@ import React from 'react';
 import PostListItem from './PostListItem';
 
 const PostList = props => {
-    const { posts, clickPost, deletePost } = props;
+    const { posts, clickPost, deletePost, editPost } = props;
     return posts.map(post => (
         <PostListItem 
             key={post.id} 
             post={post} 
-            clickPost={clickPost}
+            clickPost={clickPost} 
             deletePost={deletePost}
+            editPost={editPost}
         />
     ));
-};
+}
 
 export default PostList;
